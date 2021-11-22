3ed93f79fdaa21787be6ab7b6475f89b540a7504
Roberto Araujo <80107325+BlueMage-Code@users.noreply.github.com>
2021-11-22 01:51:09 -0300

    d01
    Utilização de oneclick e function

--- Desafios/d01/script.js
+++ Desafios/d01/script.js
@@ -1,4 +1,10 @@
 // pegar os id (b1,b2,b3) e fazer os seus respectivos popup's
-let b1 = window.document.querySelector('a,#b1');
-
-
+function popUp1(){
+    window.alert('Apertou o Botão 1')
+}
+function popUp2(){
+    window.alert('Apertou o Botão 2')
+}
+function popUp3(){
+    window.alert('Apertou o Botão 3')
+}
