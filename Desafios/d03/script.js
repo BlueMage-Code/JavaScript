b884fe48523ac56c5998ac9bdd87b1b7632e8eb9
Roberto Araujo <80107325+BlueMage-Code@users.noreply.github.com>
2021-11-23 04:42:24 -0300

    d03

---
+++ Desafios/d03/script.js
@@ -0,0 +1,6 @@
+function popUp(){
+    let basenumeral = prompt('Digite um numero')
+    basenumeral = Number(basenumeral)
+    alert(`Antes de ${basenumeral}, temos o número ${basenumeral - 1}.
+Depois de ${basenumeral}, temos o número ${basenumeral + 1}.`)
+}
