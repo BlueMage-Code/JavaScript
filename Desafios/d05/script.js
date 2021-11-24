faaa6f041d18a9b221333d81371278f302df690a
Roberto Araujo <80107325+BlueMage-Code@users.noreply.github.com>
2021-11-24 04:50:13 -0300

    d05
    Usado o Number englobando toFixed para eliminar as casa decimais não usadas caso o numero fosse inteiro.

--- Desafios/d05/script.js
+++ Desafios/d05/script.js
@@ -6,5 +6,11 @@ function metros(){
  
 function txt(m){
     let txt = document.querySelector('div#inf')
-    txt.innerHTML = `<h2>A distância de ${m} metros, corresponde a...<h2> <p>${(m * 0.001).toPrecision(3)} quilômetros (Km)</p> <p>${(m * 0.01).toPrecision(3)} hectômetros (Hm)</p> <p>${(m * 0.1).toPrecision(3)} decâmetros (Dam)</p> <p>${(m * 10).toPrecision(3)} decímetros (dm)</p> <p>${(m * 10).toPrecision(3)} centímetros (cm)</p> <p>${(m * 1000).toPrecision(3)} milímetros (mm)</p>`
+    txt.innerHTML = `<h2>A distância de ${m} metros, corresponde a...<h2>
+    <p>${Number((m * 0.001).toFixed(3))} quilômetros (Km)</p>
+    <p>${Number((m * 0.01).toFixed(3))} hectômetros (Hm)</p>
+    <p>${Number((m * 0.1).toFixed(3))} decâmetros (Dam)</p> 
+    <p>${Number((m * 10).toFixed(3))} decímetros (dm)</p>
+    <p>${Number((m * 100).toFixed(3))} centímetros (cm)</p> 
+    <p>${Number((m * 1000).toFixed(3))} milímetros (mm)</p>`
 }
