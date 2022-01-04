cdcc223453aac2534daa5f8241f1557195cc830c
Roberto Araujo <80107325+BlueMage-Code@users.noreply.github.com>
2022-01-04 02:34:55 -0300

    Formulario

--- Atividades/ex18/script.js
+++ Atividades/ex18/script.js
@@ -1,51 +1,60 @@
 let lista = []
-let listNum = document.querySelector('select#listNum')
+
 function pow(){
-  let num = document.querySelector('input#num').value
+  let reset = document.querySelector('input#num')
+  let num = Number(reset.value)
   if(num === ''){
-    alert('Digite um Valor!')
+    alert('Digite um Valor')
+    reset.focus()
   }
-  else if(  num < 1 || num > 100){
-    alert('Digite um Valor entre 1 e 100')  
+  else if(num < 1 || num > 100){
+    alert('Digite um valor entre 1 e 100')
+    reset.value = ''
+    reset.focus()
   }
   else if(lista.indexOf(num) > -1){
     alert('Esse número ja existe na lista') 
+    reset.value = ''
+    reset.focus()
   }
   else{
-      adicionar(num)
-  } 
-    
+    adicionar(reset,num)
+  }
 }
 
-function adicionar(num){  
-    lista.push(num)
-    let item = document.createElement('option')
-    item.text = `Valor ${num} foi Adicionado`
-    listNum.appendChild(item)
-   
-
+function adicionar(reset,num){
+  let listNum = document.querySelector('select#listNum')
+  lista.push(num)
+  let item = document.createElement('option')
+  item.text = `Valor ${num} foi Adicionado`
+  listNum.appendChild(item)
+  reset.value = ''
+  reset.focus()
+  con.innerHTML = ''
 }
 
 function powpow(){
-    lista.sort()
-    if(lista.length == 0){
-        alert('Adicione numeros primeiro antes de acionar o botão!')
+  let con = document.querySelector('div#con')
+  if(lista.length == 0){
+    alert('Adicione numeros primeiro antes de acionar o botão!')
+  }
+  else{
+    maior = lista[0]
+    menor = lista[0]
+    soma = 0
+    for(let i in lista){
+      soma += lista[i]
+      if(lista[i] > maior){
+        maior = lista[i]
+      }
+      if(lista[i] < menor){
+        menor = lista[i]
+      }
     }
-    else{
-        let con = document.querySelector('div#con')
-        alert(lista)
-        let soma = 0
-        for(let i = 0; i < lista.length; i++){
-            // pesquisar o porque da array nao deixar ultiliza-la sem transforma-la
-            let y = Number(lista[i])
-            soma = soma + y
-        }
-        con.innerHTML = `<p>Ao todo, temos ${lista.length} números cadastrados.</p>`
-        con.innerHTML += `<p>O maior valor informado foi ${lista[(lista.length)-1]}.</p>`
-        con.innerHTML += `<p>O menor valor informado foi ${lista[0]}.</p>`
-        con.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
-        con.innerHTML += `<p>A média dos valores digitados é ${soma / lista.length}</p>`
-    }
+  }
+    con.innerHTML = `<p>Ao todo, temos ${lista.length} números cadastrados.</p>`
+    con.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+    con.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+    con.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+    con.innerHTML += `<p>A média dos valores digitados é ${soma / lista.length}</p>`
 }
-    
-
